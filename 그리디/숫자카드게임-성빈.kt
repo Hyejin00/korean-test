4608db5d3f84d6a113f0d0f841b79a912139687b
Ji Sungbin <ji@sungb.in>
2021-06-27 23:45:12 +0900

    Update 숫자카드게임-성빈.kt

--- 그리디/숫자카드게임-성빈.kt
+++ 그리디/숫자카드게임-성빈.kt
@@ -2,5 +2,5 @@ fun main() {
     val n = readLine()!!.split(" ").first().toInt()
     val result = mutableListOf<Int>() // 이거 더 짧게 될거같은데
     repeat(n) { result.add(readLine()!!.split(" ").minOf { it.toInt() }) }
-    println(result.maxOrNull()!!)
+    println(result.toList().maxOrNull()!!)
 }
