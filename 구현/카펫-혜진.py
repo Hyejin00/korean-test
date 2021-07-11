38bdb0edadcbb2b748d23287a21aaa93b7b674f0
Hyejin00 <44718809+Hyejin00@users.noreply.github.com>
2021-07-11 23:09:30 +0900

    카펫-수정

--- 구현/카펫-혜진.py
+++ 구현/카펫-혜진.py
@@ -3,11 +3,8 @@ def solution(brown, yellow):
   end = int(brown/2)+2
   for i in range(3, end):
     if (i-2)*(end-i-2) == yellow:
-      if i > end-i:
-        answer.append(i)
-        answer.append(end-i)
-      else:
-        answer.append(end-i)
-        answer.append(i)
+      answer.append(i)
+      answer.append(end-i)
       break
+  answer.sort(reverse=True)
   return answer
